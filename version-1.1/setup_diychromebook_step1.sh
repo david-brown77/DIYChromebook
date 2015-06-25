8053b4fa2de70ad92ea81e8c47520f9ee538a737
Ralph deGennaro <rdegennaro@sscps.org>
2015-06-25 13:26:36 -0400

    99% done with policy

---
+++ version-1.1/setup_diychromebook_step1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ToDo / Remember
+# make /etc/opt/chrome/policies/managed
+# make /etc/opt/chrome/policies/recommended
+# download default_policy.json to /etc/opt/chrome/policies/managed
+
+
--- version-1.1/update_bruteforce.sh
+++ version-1.1/update_bruteforce.sh
@@ -2,8 +2,5 @@
 # quick link to master copy = http://goo.gl/V8GjhV
 apt-get update
 aptitude -y --full-resolver safe-upgrade
-# uncomment next two lines if system has Firefox installed
-#apt-get -y remove flashplugin-installer
-#apt-get -y install flashplugin-installer
 apt-get -y autoremove
 apt-get clean
