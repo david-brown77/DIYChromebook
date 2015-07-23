42026e6233efe481f8dead5e1f2a695de13b6fe1
Ralph deGennaro <rdegennaro@sscps.org>
2015-07-23 10:28:20 -0400

    Update setup_diychromebook_step1.sh

--- version-1.1/setup_diychromebook_step1.sh
+++ version-1.1/setup_diychromebook_step1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# short URL = https://goo.gl/LTQvgk, change it for (after) push to main SSCPS 
+# short URL = https://goo.gl/LTQvgk, CHANGE:  be sure to update to correct repository & make new link
 
 # ToDo / Remember
-# make /etc/opt/chrome/policies/managed
+# check to see if code setting googleuser user to automatically login works with KDE
+# set login screen background
+# modify panel, maybe in step 2 (after create home directory?)
+# find better way to add webmin repositoryy so that dups aren't added if run twice
 # make /etc/opt/chrome/policies/recommended
-# download default_policy.json to /etc/opt/chrome/policies/managed
+# define recommended policies
 
 # work in progress, use at your own risk
 # assumptions:
@@ -25,7 +28,57 @@ aptitude -y --full-resolver safe-upgrade
 # install DE & clean out unnecessary apps for DIYChromebook
 apt-get -y install kubuntu-desktop #needed only if use ubuntu server (certain iMacs, older machines)
 apt-get -y remove kpat ktorrent akregator kopete quassel firefox 
-apt-get -y install git git-doc mc mutt
 apt-get -y install ubuntu-wallpapers kde-wallpapers kde-wallpapers-default kdewallpapers peace-wallpapers tropic-wallpapers xubuntu-wallpapers
 apt-get -y install kscreensaver screensaver-default-images
 apt-get -y install kubuntu-restricted-extras
+#
+# install tools
+# setup repository for webmin
+echo "" >> /etc/apt/sources.list
+echo "# repositories added for webmin installation/update" >> /etc/apt/sources.list
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
+cd /root/setup
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add /root/setup/jcameron-key.asc
+apt-get update
+apt-get -y install webmin git git-doc mc mutt
+apt-get -f -y install
+# download script for "unattended" updating 
+cd /home/System/scripts/
+# CHANGE:  be sure to update to correct repository
+wget https://github.com/rdegennaro/DIYChromebook/blob/master/version-1.1/update_bruteforce.sh
+chmod a+x /home/System/scripts/update_bruteforce.sh
+#
+# boot splash  theme, need to be after lubuntu-desktop because that installs its own
+# might require file edits before in /etc/default/grub, should check what lubuntu-desktop does to it
+apt-get -y install plymouth-theme-edubuntu
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/g' /etc/default/grub
+update-alternatives --set default.plymouth /lib/plymouth/themes/edubuntu-logo/edubuntu-logo.plymouth
+update-grub
+update-initramfs -u
+#
+# install Google Chrome & download/install policy (policy part is BUSTED because of BUG in chrome)
+cd /root/setup
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome*.deb
+apt-get -f -y install
+mkdir -p /etc/opt/chrome/policies/managed
+cd /etc/opt/chrome/policies/managed/
+# CHANGE:  be sure to update to correct repository
+wget https://raw.githubusercontent.com/rdegennaro/DIYChromebook/master/version-1.1/default_policy.json
+#
+# clean up everything
+apt-get -y autoremove
+apt-get clean
+#
+# setup "googleuser" account for generic Google Chrome use
+# add user & password
+adduser --home /home/googleuser --shell /bin/bash --disabled-password --gecos "Google User" googleuser
+echo 'googleuser:ChangeThisToTheCorrectPassword' | chpasswd
+# TODO - check to see if below sets googleuser user to automatically login with KDE
+echo "" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+echo "#added by DIYChromebook (v2) script" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+echo "autologin-user=googleuser" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+echo "autologin-user-timeout=0" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
+echo "greeter-session=lightdm-gtk-greeter" >> /etc/lightdm/lightdm.conf.d/20-lubuntu.conf
