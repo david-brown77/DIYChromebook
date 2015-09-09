0351c965a4fccd8f8f727b3889983930f9d6fba3
Ralph deGennaro <rdegennaro@sscps.org>
2015-09-09 13:35:47 -0400

    forgot shortened URL needs to be to RAW file

--- version-1.0/setup_new_diychromebook2_delldX20.sh
+++ version-1.0/setup_new_diychromebook2_delldX20.sh
@@ -4,7 +4,7 @@
 #   2.  assumes its being run as root
 #   3.  assumes that the ubuntu-server distro is being used
 # if you are behind a webfilter and need weblogin, try w3m
-# quick link to master copy = https://goo.gl/BpZwnK
+# quick link to master copy = https://goo.gl/cJXEWD
 #
 # setup temp directory for rest of script
 mkdir -p /root/setup
