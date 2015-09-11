a1317ad6a15945a4217b94383e9f122b37c07472
Ralph deGennaro <rdegennaro@sscps.org>
2015-09-11 11:05:43 -0400

    cleanup profile & login/logout scripts, part 1

--- version-1.0/setup_new_diychromebook2_step2.sh
+++ version-1.0/setup_new_diychromebook2_step2.sh
@@ -6,17 +6,21 @@
 #   4.  this script is run AFTER the additional manual steps are done after the first script
 #
 # remove non essential directories, change Desktop so can't write to it
-chown root:root /home/googleuser/Desktop
-chmod go-rwx /home/googleuser/Desktop
+#chown root:root /home/googleuser/Desktop
+#chmod go-rwx /home/googleuser/Desktop
+rm -Rv /home/googleuser/Desktop
+ln -s /home/googleuser/Downloads /home/googleuser/Desktop
 rm -Rv /home/googleuser/Documents
 rm -Rv /home/googleuser/Music
 rm -Rv /home/googleuser/Pictures
 rm -Rv /home/googleuser/Public
 rm -Rv /home/googleuser/Templates
 rm -Rv /home/googleuser/Videos
+#
+# not doing this with simpler method
 # create the default template directory to be used for Google Chrome profile
-cp -ar /home/googleuser/.config/google-chrome /home/googleuser/.config/google-chrome-template
-chown -R root:root /home/googleuser/.config/google-chrome-template
+#cp -ar /home/googleuser/.config/google-chrome /home/googleuser/.config/google-chrome-template
+#chown -R root:root /home/googleuser/.config/google-chrome-template
 #
 # get & setup script for cleanup of googleuser Google Chrome profile
 cd /home/System/scripts/
@@ -24,10 +28,13 @@ wget https://raw.githubusercontent.com/rdegennaro/DIYChromebook/master/version-1
 chmod a+x /home/System/scripts/googleuser_cleanup.sh
 mkdir -p /home/googleuser/.googleuser-aging/logs
 #
-# setup cleanup googleuser user after logout
+# setup cleanup googleuser user  logout
 echo "" >> /usr/share/lightdm/lightdm.conf.d/50-user-session.conf
 echo "#added by DIYChromebook (v2) script" >> /usr/share/lightdm/lightdm.conf.d/50-user-session.conf
 echo "session-cleanup-script=/home/System/scripts/googleuser_cleanup.sh" >> /usr/share/lightdm/lightdm.conf.d/50-user-session.conf
+echo "" >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo "#added by DIYChromebook (v2) script" >> /etc/lightdm/lightdm-gtk-greeter.conf
+echo "session-cleanup-script=/home/System/scripts/googleuser_cleanup.sh" >> /etc/lightdm/lightdm-gtk-greeter.conf
 #
 # set googleuser_cleanup.sh be run at startup, in case hard power down.
 echo "#! /bin/bash" >> /etc/init.d/googleuser_cleanup
